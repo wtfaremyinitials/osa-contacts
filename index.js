f23c5d4fd468ae4310a40a98964e6565399ba939
Will Franzen <wtfranzen@gmail.com>
2014-12-13 17:51:01 -0600

    Support addresses and phones numbers APPLE WHY DO YOU NOT PROVIDE ARRAY.MAP ON YOUR STUPID FAUX-ARRAY ARRAYS!!!!

--- index.js
+++ index.js
@@ -40,23 +40,53 @@ var getPictureFromFile = function(id, cropped, cb) {
 var Contacts = {};
 
 Contacts.getContacts = function(filter, cb) {
+    // Dear Apple
+    // Why on earth did you not provide Array.map?
+    // WHY!!!
     osa(function(filter) {
         var Contacts = Application('Contacts');
-        var found = Contacts.people.where(filter);
+        var $found = Contacts.people.where(filter);
+        var found  = [];
 
-        var ret = [];
+        for(var i=0; i<$found.length; i++) {
+            var $contact = $found[i];
+            var  contact = {};
 
-        for(var i=0; i<found.length; i++) {
-            var contact = found[i];
-            ret.push({
-                id:       contact.id(),
-                first:    contact.firstName(),
-                last:     contact.lastName(),
-                nickname: contact.nickname(),
-            });
+            contact.id       = $contact.id();
+            contact.first    = $contact.firstName();
+            contact.last     = $contact.lastName();
+            contact.nickname = $contact.nickname();
+
+            var $addresses = $found.addresses;
+            var  addresses = [];
+
+            for(var a=0; a<$addresses.length; a++) {
+                var $address = $addresses[a];
+                addresses.push({
+                    formattedAddress:   $address.formattedAddress(),
+
+                    city:    $address.city()[0],
+                    street:  $address.street()[0],
+                    zip:     $address.zip()[0],
+                    country: $address.country()[0],
+                    state:   $address.state()[0]
+                });
+            }
+
+            contact.addresses = addresses;
+
+            var $phones = $found.phones;
+            var  phones = [];
+
+            for(var p=0; p<$phones.length; p++)
+                phones.push($phones[p].value()[0]);
+
+            contact.phones = phones;
+
+            found.push(contact);
         }
 
-        return ret;
+        return found;
     }, filter, cb);
 };
 
